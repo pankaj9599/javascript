476a98d9e596832d0612051ff45fcc6cd1d206d5
pankaj9599 <pank959906@gmail.com>
2024-06-10 18:57:59 +0000

    conversion

---
+++ javascript/datatypeconversion.js
@@ -0,0 +1,39 @@
+console.log("conversion")
+// let score;
+// console.log(typeof score)
+// let chan=Number(score)
+// console.log(chan)
+// console.log(typeof chan)
+
+/*
+33=>33 (number)
+33a=>nan(string)numberbut its not
+null=0(object)number
+undefined. (undefined)number
+*/
+
+
+// let isloggedin=null ;
+// let bool=Boolean(isloggedin)
+// console.log(bool)
+
+
+
+// let a=3;
+// let b=++a;
+// console.log(a,b);
+
+// console.log(a+b);
+
+// let c="adaaaa";
+// let d="adad"
+// console.log(c+d);
+
+// console.log(1+2+"6");
+// console.log(1+2+"2"+2);
+// console.log("1"+2+4);
+
+
+// comparision
+console.log(2>"1");
+console.log("02">1);
