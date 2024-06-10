bb78a390bcd864d5397b006f5f3905da639dfd9c
pankaj9599 <pank959906@gmail.com>
2024-06-10 09:16:48 +0000

    data

---
+++ javascript/02data.js
@@ -0,0 +1,7 @@
+console.log(4+6)
+console.log("hi")
+let name="pankaj"
+let a=5
+let s=null
+let n=true
+console.log(name,a,s,n)
---
+++ javascript/basics.js
@@ -0,0 +1,7 @@
+console.log("hello")
+const c="pankaj@gmail.com"
+let a="pankaj kumar"
+var account=1223
+account =2
+// use of var is not recommended now
+console.log(c,a,account)
