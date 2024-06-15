c2905a379c7aeacf4e9655dcba67c44f671fecf0
pankaj_kumar <pank959906@gmail.com>
2024-06-15 19:13:11 +0000

    done

---
+++ javascript/date.js
@@ -0,0 +1,11 @@
+console.log("DATE");
+
+let mydate=new Date();
+// console.log(`this the date ->${mydate}`);
+console.log(mydate.toString());
+console.log(mydate.toISOString());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleTimeString());
+console.log(typeof mydate);
---
+++ javascript/number.js
@@ -0,0 +1,29 @@
+// console.log("number and maths");
+// ++++++++++++number+++++++++++++++++++++
+let score=400;
+// console.log(`the score is ${score}`);
+let gst=new Number(50);
+// console.log(`the gst is ${gst}`);
+// console.log(gst.toString().length);
+// console.log(gst.toFixed(2));
+let profit=2233.394;
+// console.log(profit.toPrecision(5))
+let sno=234635768768678;
+// console.log(sno.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++maths++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.2));
+
+
+// in random values come btw 0 to 1;
+// console.log(Math.random());
+
+const max=220;
+const min=10;
+
+console.log(Math.floor(Math.random()*(max-min+1)+min));
