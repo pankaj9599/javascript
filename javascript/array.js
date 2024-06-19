057355c8fe4f580873e388251c0a8df7a15e9a81
pankaj_kumar <pank959906@gmail.com>
2024-06-19 11:41:54 +0000

    array

--- javascript/array.js
+++ javascript/array.js
@@ -5,8 +5,19 @@ console.log(" ARRAY");
 // creating array without new keyword
 const arr=["swift","honda","thar"];
 // creating array using new keyword 
-const arr1=new Array[3,5,7,8,2];
+// const arr1=new Array[3,5,7,8,2];
 
 // The two examples above do exactly the same.
 // There is no need to use new Array().
 // For simplicity, readability and execution speed, use the array literal method.
+
+const marval=["spider","hulk","ironman"];
+const dc=["superman","batman","wolverine"]
+// console.log(marval);
+// marval.push(dc);
+// console.log(marval);
+
+const con=marval.concat(dc);
+// console.log(con);
+const he=[...marval,...dc];
+console.log(he);
