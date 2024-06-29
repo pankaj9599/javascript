62b0b0fa2d29d8733d1fe1639082365a4e49b1e2
pankaj_kumar <pank959906@gmail.com>
2024-06-29 12:03:46 +0000

    joij

---
+++ javascript/function.js
@@ -0,0 +1,18 @@
+console.log("function");
+
+
+function myname(){
+    console.log("pankaj");
+    console.log("kumar");
+
+}
+// myname();
+
+
+// sum of two Number
+
+function sum(number1,number2){
+   console.log(number1+number2) ;
+}
+
+sum(4,3);
