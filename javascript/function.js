6a6516a56be9070fe716f33eb3aea2d48073f716
pankaj_kumar <pank959906@gmail.com>
2024-06-30 07:05:13 +0000

    j

--- javascript/function.js
+++ javascript/function.js
@@ -15,4 +15,39 @@ function sum(number1,number2){
    console.log(number1+number2) ;
 }
 
-sum(4,3);
+// sum(4,3);
+
+// shoping cart 
+
+function shopingcart(...number1){
+    return number1;
+}
+// it return the array 
+// console.log(shopingcart(2,3,6,8));
+
+
+const user={
+    username:"pankaj",
+    age:21,
+}
+
+function handleuser(userss){
+    console.log(`user name ${userss.username} and age is ${userss.age}`);
+}
+// passing function;
+// handleuser(user);
+
+handleuser({
+username:"sam",
+age:23,
+});
+
+
+// now passing array 
+const array=[2,3,4,5,6];
+function print(getarray){
+return getarray[2];
+}
+// console.log(print(array));
+
+console.log(print([1,2,3,4,5]));
