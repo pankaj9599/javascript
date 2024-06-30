f41a484d96d3234e264e18c83b8e571fb5e80e7b
pankaj_kumar <pank959906@gmail.com>
2024-06-30 16:23:46 +0000

    f

---
+++ javascript/global.js
@@ -0,0 +1,13 @@
+console.log("GLOBAL");
+let a=300;
+
+if(true){
+    let a=3;
+    const d=7;
+    // var c=4;
+    console.log(`inner ,${a}`);
+
+}
+console.log(a);
+// console.log(d);
+// console.log(c);`1```
