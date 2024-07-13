08a1acd78f9a8dfe6246d7eb7c02de3a473a0881
pankaj_kumar <pank959906@gmail.com>
2024-07-13 15:20:49 +0000

    task1

---
+++ javascriptprectice/task_1.js
@@ -0,0 +1,40 @@
+//  variable declaration 
+var id=34;
+// console.log(id);
+
+let rollno="pankaj";
+// console.log(rollno);
+
+const value=true;
+// console.log(value);
+
+
+
+let vari1 = 4;
+let vari2= "gkh";
+let vari3 = true;
+let vari4 = {};
+let vari5 = [];
+// console.log(typeof  vari1);
+// console.log(typeof vari2);
+// console.log(typeof vari3);
+// console.log(typeof vari4);
+// console.log(typeof vari5);
+
+let val1=7;
+// console.log(val1);
+val1=48;
+// console.log(val1);
+
+
+const var1=34;
+console.log(var1);
+// var1=45;
+console.log(var1);
+
+
+let valu1=3;
+console.log(typeof valu1);
+valu1="pankaj"
+console.log(typeof valu1);
+
