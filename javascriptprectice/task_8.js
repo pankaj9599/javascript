c453940c054c5a7c022a6295d51ca99e4c367e93
pankaj_kumar <pank959906@gmail.com>
2024-07-21 17:32:54 +0000

    j

--- javascriptprectice/task_8.js
+++ javascriptprectice/task_8.js
@@ -1,16 +1,45 @@
-const fr={
-    gamename:"invincible",
-    rollno:21,
-    author:"vinchi",
-    book:"carlo",
-    bookplusauthor:function(){
-        return this.book +" "+this.author;
-    },
-    chanderollno:function(rollno){
-        return this.rollno;
-    }
+// const fr={
+//     gamename:"invincible",
+//     rollno:21,
+//     author:"vinchi",
+//     book:"carlo",
+//     bookplusauthor:function(){
+//         return this.book +" "+this.author;
+//     },
+//     chanderollno:function(rollno){
+//         return this.rollno;
+//     }
+// }
+// // console.log(fr);
+// // console.log(fr.book);
+// // console.log(fr.bookplusauthor());
+
+// fr.rollno=54;
+// console.log(fr.chanderollno());
+
+const library ={
+person1:{
+    name:"pankaj",
+    book:[1,2,3,4],
+
+},
+person2:{
+    name:"rahul",
+    book:[1,2,3,7],
+},
+person3:{
+    name:"ajay",
+    book:[1,2,3,5],
+},
 }
-// console.log(fr);
-// console.log(fr.book);
-// console.log(fr.bookplusauthor());
-console.log(fr.chanderollno(32));
+// console.log(library.person1.book);
+// console.log(library.person2.name);
+// console.log(library.person3.name);
+
+for (const it in library) {
+    for (const key in it) {
+       
+        console.log(it[key]);
+            
+        }
+    }
